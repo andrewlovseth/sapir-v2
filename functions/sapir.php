d65dbc441f4df70c248eaf65349005e8f0016b90
Andrew Lovseth <andrew.lovseth@me.com>
2023-11-03 10:31:01 -0700

    a.m. p.m.styling

--- functions/sapir.php
+++ functions/sapir.php
@@ -3,7 +3,13 @@
 function wrap_sapir_in_span( $content ) {
     // Use preg_replace with the 'i' modifier for case-insensitive matching.
     $content = preg_replace( '/\bsapir\b/i', '<span class="small-caps">$0</span>', $content );
+
+
+    $pattern = '/(A\.M\.|P\.M\.)/i';
+    $replacement = '<span class="small-caps lower">$0</span>';
+    $content = preg_replace( $pattern, $replacement, $content );
+
     return $content;
 }
+add_filter( 'the_content', 'wrap_sapir_in_span', 10 );
 
-add_filter( 'the_content', 'wrap_sapir_in_span' );

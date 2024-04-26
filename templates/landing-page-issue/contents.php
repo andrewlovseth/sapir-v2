85011ab01e334406bf56702373048a8c789d0cd6
Andrew Lovseth <andrew.lovseth@me.com>
2024-04-26 16:33:48 -0700

    LP and issue styling refinements

--- templates/landing-page-issue/contents.php
+++ templates/landing-page-issue/contents.php
@@ -26,31 +26,35 @@
     </div>
 
     <h4 class="issue__contents-header | upper-header">Contents of the Issue</h4>
+
+    <div class="issue__contents-body">
+        <?php if(have_rows('table_of_contents', $issue->ID)): while(have_rows('table_of_contents', $issue->ID)) : the_row(); ?>
+
+            <?php if( get_row_layout() == 'section' ): ?>
+                <?php
+                    $header = get_sub_field('heading'); 
+                ?>
+
+                <div class="issue__contents-section">
+                    <?php if($header):?>
+                        <h2 class="issue__contents-header upper-header"><?php echo $header; ?></h2>
+                    <?php endif; ?>
+
+                    <?php if(have_rows('articles')):  while(have_rows('articles')): the_row(); ?>
+
+                        <?php
+                            $article = get_sub_field('article');
+                            $args = ['p' => $article->ID];
+                            get_template_part('template-parts/global/teaser/teaser-contents', null, $args);
+                        ?>
+
+                    <?php endwhile; endif; ?>
+                </div>
+
+            <?php endif; ?>
+        
+        <?php endwhile; endif; ?>
+    </div>
     
-    <?php if(have_rows('table_of_contents', $issue->ID)): while(have_rows('table_of_contents', $issue->ID)) : the_row(); ?>
 
-        <?php if( get_row_layout() == 'section' ): ?>
-            <?php
-                $header = get_sub_field('heading'); 
-            ?>
-
-            <div class="issue__contents-section">
-                <?php if($header):?>
-                    <h2 class="issue__contents-header upper-header"><?php echo $header; ?></h2>
-                <?php endif; ?>
-
-                <?php if(have_rows('articles')):  while(have_rows('articles')): the_row(); ?>
-
-                    <?php
-                        $article = get_sub_field('article');
-                        $args = ['p' => $article->ID];
-                        get_template_part('template-parts/global/teaser/teaser-contents', null, $args);
-                    ?>
-
-                <?php endwhile; endif; ?>
-            </div>
-
-        <?php endif; ?>
-    
-    <?php endwhile; endif; ?>
 </div>
--- templates/single-issue/contents.php
+++ templates/single-issue/contents.php
@@ -1,28 +1,29 @@
 <div class="issue__contents">
-    <?php if(have_rows('table_of_contents')): while(have_rows('table_of_contents')) : the_row(); ?>
+    <div class="issue__contents-body">
+        <?php if(have_rows('table_of_contents')): while(have_rows('table_of_contents')) : the_row(); ?>
 
-        <?php if( get_row_layout() == 'section' ): ?>
-            <?php
-                $header = get_sub_field('heading'); 
-            ?>
+            <?php if( get_row_layout() == 'section' ): ?>
+                <?php
+                    $header = get_sub_field('heading'); 
+                ?>
+            
+                <div class="issue__contents-section">
+                    <?php if($header):?>
+                        <h2 class="issue__contents-header upper-header"><?php echo $header; ?></h2>
+                    <?php endif; ?>
 
-            <div class="issue__contents-section">
-                <?php if($header):?>
-                    <h2 class="issue__contents-header upper-header"><?php echo $header; ?></h2>
-                <?php endif; ?>
+                    <?php if(have_rows('articles')):  while(have_rows('articles')): the_row(); ?>
 
-                <?php if(have_rows('articles')):  while(have_rows('articles')): the_row(); ?>
+                        <?php
+                            $article = get_sub_field('article');
+                            $args = ['p' => $article->ID];
+                            get_template_part('template-parts/global/teaser/teaser-contents', null, $args);
+                        ?>
 
-                    <?php
-                        $article = get_sub_field('article');
-                        $args = ['p' => $article->ID];
-                        get_template_part('template-parts/global/teaser/teaser-contents', null, $args);
-                    ?>
+                    <?php endwhile; endif; ?>
+                </div>
+            <?php endif; ?>
 
-                <?php endwhile; endif; ?>
-            </div>
-
-        <?php endif; ?>
-    
-    <?php endwhile; endif; ?>
+        <?php endwhile; endif; ?>
+    </div>
 </div>

ce23e3799ed2af42860543dc1f8c4f5bb2e44c2b
Andrew Lovseth <andrew.lovseth@me.com>
2024-11-17 14:27:13 -0800

    Update TOC post object to sort by date

--- functions/acf.php
+++ functions/acf.php
@@ -40,3 +40,15 @@ function bearsmith_acf_styles() {
     <?php
 }
 add_action('acf/input/admin_head', 'bearsmith_acf_styles');
+
+// ... existing code ...
+
+// Order Post Object field by date
+function bearsmith_post_object_order_by_date($args, $field, $post_id) {
+    if ($field['key'] === 'field_606e240de3df9') {
+        $args['orderby'] = 'date';
+        $args['order'] = 'DESC';
+    }
+    return $args;
+}
+add_filter('acf/fields/post_object/query', 'bearsmith_post_object_order_by_date', 10, 3);
